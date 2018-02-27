c5479ac24dfc307414d46cdc9843573c9ced9a16
Diana Borbe <dborbe@axway.com>
2018-02-27 16:22:45 -0500

    INT - Configure cassandra db when executing startup script.

--- run.sh
+++ run.sh
@@ -5,13 +5,15 @@ shopt -s nullglob
 echo "-----Starting Messagerie application!-----"
 
 # use compose to start cassandra and redis
-#docker-compose up -D
+docker-compose up -d
 
 # sleep in order to give time to services to start
 echo "-----Waiting for services to start!-----"
 sleep 30
 
-# configure cassandra. TODO
+# configure cassandra.
+docker cp src/main/resources/statements.sql cassandra-container:/home/
+docker exec -it cassandra-container cqlsh -f /home/statements.sql
 
 # start application
 docker run -p 8080:8080 --detach -t --network=messagerie_internal-network messagerie:1 
