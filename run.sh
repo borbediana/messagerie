c9d00cb9eacc986f3b66cae805acdf88cefea4db
Diana Borbe <dborbe@axway.com>
2018-02-27 16:03:05 -0500

    INT - Update run.sh, tie application container to the services network

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ shopt -s nullglob
 echo "-----Starting Messagerie application!-----"
 
 # use compose to start cassandra and redis
-docker-compose up -D
+#docker-compose up -D
 
 # sleep in order to give time to services to start
 echo "-----Waiting for services to start!-----"
@@ -13,7 +13,7 @@ sleep 30
 
 # configure cassandra. TODO
 
-# start application, TODO find a detached mode
-docker run -p 8080:8080 -it messagerie:1
+# start application
+docker run -p 8080:8080 --detach -t --network=messagerie_internal-network messagerie:1 
 echo "-----Messagerie ready!-----"
 
