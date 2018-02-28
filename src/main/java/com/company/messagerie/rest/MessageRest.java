df73e328fc43016c39955815aa61d030de2ed053
Diana Borbe <dborbe@axway.com>
2018-02-28 10:24:14 +0200

    INT - Fix payload for create message REST endpoint

--- src/main/java/com/company/messagerie/rest/MessageRest.java
+++ src/main/java/com/company/messagerie/rest/MessageRest.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,7 +26,7 @@ public class MessageRest {
 	private BusinessService businessService;
     
     @RequestMapping(value = "/create", method = RequestMethod.PUT)
-    public ResponseEntity<String> createMessage(MessageRequest message) {
+    public ResponseEntity<String> createMessage(@RequestBody MessageRequest message) {
     	if(message.getType() == null) {
     		message.setType(MessageType.CHAT);
     	}
